3d99c4e0c58c59634eabf6897b95420935a268bf
kaakaa <stooner.hoe@gmail.com>
2021-02-10 21:20:16 +0900

    problems 28

---
+++ problems/28/java/Solution.java
@@ -0,0 +1,7 @@
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Implement strStr().
+// Memory Usage: 37.6 MB, less than 73.54% of Java online submissions for Implement strStr().
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
